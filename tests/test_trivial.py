4c5be8ab42f4fcba9f6153d071c303d77a51e3cb
ELB <elb@chem.ku.dk>
2025-04-24 12:57:10 +0200

    Added trivial test that should never break.
    Tested that pytest can ran find the testfile and run it fine

---
+++ tests/test_trivial.py
@@ -0,0 +1,6 @@
+"""Trivial test that should never fail"""
+
+
+def test() -> None:
+    """If this test fails, then something has really fucked up."""
+    assert True, "Something has really been fucked up."
